66365d44e830733f6373444305ccb205f5411fea
VectorZhou <871041532@outlook.com>
2021-01-21 18:23:39 +0800

    GL直接画线

---
+++ Assets/Learn/MathLearn/GLPaint.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1、首先new一个添加点用来绘制；
+// 2、一个创建材质的函数，创建绘制需要的材质；
+// 3、在 OnRenderObject() 函数绘制线段和平面；
+// 4、分别创建画线和绘制平面
+public class GLPaint : MonoBehaviour
+{
+    private List<Vector3> m_tempList;  // 存放点的列表
+    void Start()
+    {
+        m_tempList = new  List<Vector3>();
+        m_tempList.Add(new Vector3(0, 0, 0));
+        m_tempList.Add(new Vector3(0, 3, 0));
+        m_tempList.Add(new Vector3(3, 3, 0));
+        m_tempList.Add(new Vector3(3, 0, 0));
+        CreateLineMaterial();
+    }
+
+    public Material lineMaterial;
+
+    void CreateLineMaterial()
+    {
+        if (lineMaterial == null)
+        {
+            Shader shader = Shader.Find("MathLearn/GLPaintShader");
+            lineMaterial = new Material(shader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+//            lineMaterial.SetColor("m_Color", Color.yellow);
+            
+        }
+    }
+
+    private void OnRenderObject()
+    {
+        lineMaterial.SetPass(0);
+        DrawTriangle(m_tempList);
+    }
+
+    /// GL画线函数
+    void DrawLines(List<Vector3> tmpBorderPoints)
+    {
+        GL.PushMatrix();
+        // Draw lines
+        GL.Begin(GL.LINES);
+        // Vertex colors change from red to green
+        GL.Color(Color.red);
+        if (tmpBorderPoints != null)
+        {
+            for (int i = 0; i < tmpBorderPoints.Count; i++)
+            {
+                Vector3 posOne = tmpBorderPoints[i]; 
+                Vector3 posTwo = tmpBorderPoints[(i + 1) % tmpBorderPoints.Count];         
+                // One vertex at transform position
+                GL.Vertex3(posOne.x, posOne.y, posOne.z);
+                // Another vertex at edge of circle
+                GL.Vertex3(posTwo.x, posTwo.y, posTwo.z);
+            }
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    // GL 平面绘制
+    void DrawTriangle(List<Vector3> tmpBorderPoints)
+    {
+        GL.PushMatrix();
+        // Set transformation matrix for drawing to
+        // match our transform
+        GL.MultMatrix(transform.localToWorldMatrix);
+        // Draw lines
+        GL.Begin(GL.TRIANGLES);
+        // Vertex colors change from red to green
+        GL.Color(Color.green);
+        if (tmpBorderPoints != null)
+        {
+            for (int i = 0; i < tmpBorderPoints.Count; i++)
+            {
+                if (i < tmpBorderPoints.Count - 2)
+                {
+                    // 以第一个为原点，绘制三角形面
+                    Vector3 posZero = tmpBorderPoints[0];
+                    Vector3 posOne = tmpBorderPoints[i + 1];
+                    Vector3 posTwo = tmpBorderPoints[(i + 2)];
+                    GL.TexCoord2(0, 0);
+                    GL.Vertex3(posZero.x, posZero.y, posZero.z);
+                    GL.TexCoord2(0, 1);
+                    GL.Vertex3(posOne.x, posOne.y, posOne.z);
+                    GL.TexCoord2(1, 1);
+                    GL.Vertex3(posTwo.x, posTwo.y, posTwo.z);
+                }
+            }
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Learn/MathLearn/GLPaintShader.shader
@@ -0,0 +1,74 @@
+﻿Shader "MathLearn/GLPaintShader"
+{
+    Properties {
+           m_Color("Color Tint", Color) = (1, 1, 1, 1)
+           m_MainTex("Main Tex", 2D) = "white" {} 
+           m_Specular("Specular", Color) = (1, 1, 1, 1)
+           m_Gloss("Gloss", Range(8.0, 256)) = 20
+    }
+    
+    SubShader{
+        Tags{"LightMode"="ForwardBase"}
+        Pass {
+             CGPROGRAM
+             #include "UnityCG.cginc"
+             #include "Lighting.cginc"
+             #pragma vertex vert
+             #pragma fragment frag
+             
+             float m_Gloss;
+             fixed4 m_Specular;
+             fixed4 m_Color;
+             float4 m_MainTex_ST;
+             sampler2D m_MainTex;
+             
+             struct a2v {
+                float4 vertex: POSITION;
+                float3 normal: NORMAL;
+                float4 texcoord: TEXCOORD0;
+             };
+             
+             struct v2f
+             {
+                float4 pos:SV_POSITION;
+                float3 worldNormal:TEXCOORD0;
+                float3 worldPos:TEXCOORD1;
+                float2 uv:TEXCOORD2;
+             };
+             
+             v2f vert(a2v v)
+             {
+                 v2f o;
+                 o.pos = UnityObjectToClipPos(v.vertex);
+                 o.worldNormal = UnityObjectToWorldNormal(v.normal);
+                 o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+                 // 计算最终uv坐标，先缩放再偏移，可以使用宏TRANSFORM_TEX(v.texcoord, m_MainTex)
+                 o.uv = v.texcoord.xy * m_MainTex_ST.xy + m_MainTex_ST.zw; 
+                 return o;
+             }
+             
+             fixed4 frag(v2f i): SV_Target
+             {
+                 // fixed3 worldLight = normalize(_WorldSpaceLightPos0.xyz);
+                  fixed3 worldLight = normalize(UnityWorldSpaceLightDir(i.worldPos));
+                  // 漫反射
+                  // 对纹理采样，替代漫反射颜色
+                  fixed3 albedo = tex2D(m_MainTex, i.uv).rgb * m_Color.rgb;
+                  fixed3 diffuse = _LightColor0.rgb * albedo; //* max(0, dot(i.worldNormal, worldLight));
+                  // 环境光也受abledo影响
+                  fixed3 ambient = UNITY_LIGHTMODEL_AMBIENT.xyz * albedo;
+                  //高光
+                  fixed3 viewDir = normalize(_WorldSpaceCameraPos.xyz - i.worldPos);
+                  fixed3 halfDir = normalize(worldLight + viewDir);
+                  fixed3 specular = _LightColor0.rgb * m_Specular.rgb * pow(saturate(dot(i.worldNormal, halfDir)), m_Gloss);
+                  fixed4 color = fixed4((ambient + diffuse + specular), 1.0);
+//                  color = m_Color;
+                  return color;
+             }
+             ENDCG      
+        }
+    }
+    
+    Fallback Off
+}
+ 
